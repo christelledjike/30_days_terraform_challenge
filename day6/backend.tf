95c12c6766d4397a70a851035d8bf26275c2fc00
christelledjike <ngassamchristelle@hotmail.com>
2026-03-31 14:05:07 -0500

    day update: terraform backend + cleanup

---
+++ day6/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-bucket-christelle"
+    key            = "global/30days-challenge/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-30days-locks"
+    encrypt        = true
+    }
+}
+resource "null_resource" "delay" {
+  triggers = {
+    always_run = timestamp()  # changes each run
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 30"  # slow resource to hold lock
+  }
+}
+
+
